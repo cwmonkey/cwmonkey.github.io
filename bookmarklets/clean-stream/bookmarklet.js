0857f1ebce8e704d4ac0b8eb1f7303e3634e3c69
cwmonkey <wackoburns@hotmail.com>
2014-04-04 18:17:52 -0700

    fixing typo

--- bookmarklets/clean-stream/bookmarklet.js
+++ bookmarklets/clean-stream/bookmarklet.js
@@ -7,4 +7,4 @@ window.cwmBookmarkletUrl = window.cwmBaseUrl + "/bookmarklets/clean-stream";
 {% include js/jcors-loader.js %}
 {% include js/cwm-jsload.js %}
 
-{% include js/clean-stream.js %}
+{% include js/load-clean-stream.js %}
