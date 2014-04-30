93a45b62770a3678b2fe585442a48540eb7de327
cwmonkey <wackoburns@hotmail.com>
2014-04-30 13:25:06 -0700

    testing minify build.

--- bookmarklets/place-image/bookmarklet.js
+++ bookmarklets/place-image/bookmarklet.js
@@ -4,8 +4,8 @@
 window.cwmBaseUrl = "//{{ site.domain }}";
 window.cwmBookmarkletUrl = window.cwmBaseUrl + "/bookmarklets/place-image";
 
-{% include js/place-image.js %}
+{% include js/place-image.min.js %}
 
-{% include js/cwm-jsload.js %}
+{% include js/cwm-jsload.min.js %}
 
-{% include js/load-place-image.js %}
+{% include js/place-image-load.min.js %}
