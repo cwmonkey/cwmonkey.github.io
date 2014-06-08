c279b6752e8d2a1e61aac37a8649171075adc4c6
cwmonkey <wackoburns@hotmail.com>
2014-06-07 23:09:08 -0700

    Scaling images to look nicer, changing to http

--- bookmarklets/start-page/start-page.js
+++ bookmarklets/start-page/start-page.js
@@ -13,8 +13,52 @@ window.cwmStartPage.load = function (window, $, undefined) {
 	var main = function() {
 		var check_forward = function() {
 			if ( !thumb || !favicon ) return;
-			document.location = ('https:' + window.cwmBookmarkletUrl + '/start-page.html#href=' + encodeURIComponent(document.location.href) + '&title=' + encodeURIComponent(document.title) + '&thumb=' + encodeURIComponent(thumb) + '&icon=' + encodeURIComponent(favicon));
+			document.location = ('http:' + window.cwmBookmarkletUrl + '/start-page.html#href=' + encodeURIComponent(document.location.href) + '&title=' + encodeURIComponent(document.title) + '&thumb=' + encodeURIComponent(thumb) + '&icon=' + encodeURIComponent(favicon));
 		};
+
+		// http://stackoverflow.com/questions/18761404/how-to-scale-images-on-a-html5-canvas-with-better-interpolation
+		var scale = function(destCanvas, destWidth, destHeight) {
+			// var start = new Date().getTime();
+			var scalingSteps = 0;
+			var destCtx = destCanvas.getContext('2d');
+
+			var canvas = document.createElement('canvas');
+			var ctx = canvas.getContext('2d');
+			canvas.width = destCanvas.width;
+			canvas.height = destCanvas.height;
+
+			ctx.putImageData(destCtx.getImageData(0, 0, destCanvas.width, destCanvas.height), 0, 0);
+
+			var curWidth = canvas.width;
+			var curHeight = canvas.height;
+
+			var lastWidth = canvas.width;
+			var lastHeight = canvas.height;
+
+			var end = false;
+			var scale = 0.75;
+			while ( end == false ) {
+				scalingSteps += 1;
+				curWidth *= scale;
+				curHeight *= scale;
+
+				if ( curWidth < destWidth ) {
+					curWidth = destWidth;
+					curHeight = destHeight;
+					end = true;
+				}
+
+				ctx.drawImage(canvas, 0, 0, Math.round(lastWidth), Math.round(lastHeight), 0, 0, Math.round(curWidth), Math.round(curHeight));
+				lastWidth = curWidth;
+				lastHeight = curHeight;
+			}
+
+			destCanvas.width = destWidth;
+			destCanvas.height = destHeight;
+			destCtx.putImageData(ctx.getImageData(0, 0, destCanvas.width, destCanvas.height), 0, 0);
+			// var endTime = new Date().getTime();
+			//console.log("execution time: "+ ( endTime - start) + "ms. scale per frame: "+scale+ " scaling step count: "+scalingSteps);
+		}
 
 		html2canvas(document.body, {
 			useCORS: true,
@@ -44,9 +88,13 @@ window.cwmStartPage.load = function (window, $, undefined) {
 					canvasCopy.height = img.height;
 					copyContext.drawImage(img, 0, 0);
 
-					canvas.width = maxWidth;
-					canvas.height = maxHeight;
-					ctx.drawImage(canvasCopy, 0, 0, wR, hR, 0, 0, maxWidth, maxHeight);
+					//canvas.width = maxWidth;
+					//canvas.height = maxHeight;
+					canvas.width = wR;
+					canvas.height = hR;
+					//ctx.drawImage(canvasCopy, 0, 0, wR, hR, 0, 0, maxWidth, maxHeight);
+					ctx.drawImage(canvasCopy, 0, 0, wR, hR, 0, 0, wR, hR);
+
 					try {
 						thumb = canvas.toDataURL('image/gif');
 					} catch (e) {
