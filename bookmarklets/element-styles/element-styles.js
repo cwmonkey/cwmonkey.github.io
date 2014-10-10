804f0ffa83f4c5f50b35fd40e3000a4579ee6e58
Gerald burns <gerald@ziprecruiter.com>
2014-10-10 15:21:38 -0700

    Updating loops to be much faster. Specifically for short.less.

--- bookmarklets/element-styles/element-styles.js
+++ bookmarklets/element-styles/element-styles.js
@@ -3,64 +3,63 @@ window.cwmElementStyles = window.cwmElementStyles || {};
 window.cwmElementStyles.load = function (window, $, undefined) {
 	'use strict';
 
-	
-	function css(a) {
-		var sheets = document.styleSheets, o = {};
-		var cssText = '';
+	var rules = {};
+	var set_rules = function() {
+		var rule;
+		var sheets = document.styleSheets;
 		for (var i in sheets) {
-			var rules = sheets[i].rules || sheets[i].cssRules;
-			for (var r in rules) {
-				if ( undefined !== rules[r].selectorText && rules[r].selectorText.match(/^\.[a-zA-Z0-9]+$/) && a.is(rules[r].selectorText) ) {
-					cssText += "\n\t" + '/* ' + sheets[i].href + ' : ' + rules[r].selectorText + ' */' + "\n";
-					cssText += "\t" + rules[r].style.cssText.split('; ').join(";\n\t") + "\n";
-					//o = $.extend(o, css2json(rules[r].style) /*, css2json(a.attr('style')) */ );
+			var rs = sheets[i].rules || sheets[i].cssRules;
+			for (var r in rs) {
+				if (
+					undefined !== rs[r].selectorText
+					&& rs[r].selectorText.match(/^\.[a-zA-Z0-9]+$/)
+					&& rs[r].style.cssText.match(/!important/)
+				) {
+					rule = {
+						classCheck: new RegExp('\\b' + rs[r].selectorText.substring(1) + '\\b'),
+						//selectorText: rules[r].selectorText,
+						//href: sheets[i].href,
+						cssText: "\t" + rs[r].style.cssText.split('; ').join(";\n\t") + "\n",
+						comment: ''
+						//comment: "\n\t" + '/* ' + sheets[i].href + ' : ' + rules[r].selectorText + ' */' + "\n"
+					};
+
+					rules[rs[r].selectorText] = rule;
 				}
 			}
 		}
-		//return o;
+	}
+
+	function css(a) {
+		var sheets = document.styleSheets;
+		var cssText = '';
+		var classCheck;
+
+		for (var r in rules) {
+			if (
+				a.className.match(rules[r].classCheck)
+			) {
+				cssText += rules[r].comment + rules[r].cssText;
+			}
+		}
 
 		if ( !cssText ) return;
 
-		var el = a[0];
+		var el = a;
 		var selector = '';
-		if ( el.id ) {
-			selector = el.tagName + '#' + el.id;
-		} else if ( el.className ) {
-			selector = el.tagName + '.' + el.className.split(' +').join('.');
-		} else {
-			selector = el.tagName;
-		}
-
+		selector = el.tagName + '.' + el.className.split(/\s+/).join('.');
 
 		return selector + '{' + "\n" + cssText + '}' + "\n";
 	}
 
-	function css2json(css) {
-		var s = {};
-		if (!css) return s;
-		if (css instanceof CSSStyleDeclaration) {
-			for (var i in css) {
-				if ((css[i]).toLowerCase) {
-					s[(css[i]).toLowerCase()] = (css[css[i]]);
-				}
-			}
-		} else if (typeof css == "string") {
-			css = css.split("; ");
-			for (var i in css) {
-				var l = css[i].split(": ");
-				s[l[0].toLowerCase()] = (l[1]);
-			}
-		}
-		return s;
-	}
-
 	this.main = function() {
-		var $els = $('body, body *');
+		var $els = $('body[class], body *[class]');
 		var res = '';
 		var ret = '';
+		set_rules();
 		for ( var i = 0; i < $els.length; i++ ) {
 			if ( !$els[i].className ) continue;
-			ret = css($els.eq(i));
+			ret = css($els[i]);
 			console.log( ret );
 			if ( undefined !== ret ) res += "\n" + ret;
 		}
