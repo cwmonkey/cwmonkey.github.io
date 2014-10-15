a121cc340f21f17671ce403b56125ef33d73b6f7
Gerald burns <gerald@ziprecruiter.com>
2014-10-15 14:32:28 -0700

    Fixing duplicate selectors. adding (commented) code which is non-short

--- bookmarklets/element-styles/element-styles.js
+++ bookmarklets/element-styles/element-styles.js
@@ -4,6 +4,74 @@ window.cwmElementStyles.load = function (window, $, undefined) {
 	'use strict';
 
 	var rules = {};
+	var rules_done = {};
+
+	/*var set_rules = function() {
+		var rule;
+		var sheets = document.styleSheets;
+		for (var i in sheets) {
+			var rs = sheets[i].rules || sheets[i].cssRules;
+			for (var r in rs) {
+				if (
+					undefined !== rs[r].selectorText
+				) {
+					if ( rs[r].selectorText === '*' || rs[r].selectorText.match(/\:/) || rs[r].selectorText.match(/^[a-zA-Z]+$/) ) {
+						console.log( rs[r].selectorText + "{\n" + rs[r].style.cssText + "\n}" );
+						continue;
+					}
+
+					rule = {
+						cssText: "\t" + rs[r].style.cssText.split('; ').join(";\n\t") + "\n",
+						element: rs[r].selectorText.match(/ [a-zA-Z]+$/),
+						done: false
+						//comment: "\n\t" + '/* ' + sheets[i].href + ' : ' + rules[r].selectorText + ' *\/' + "\n"
+					};
+
+					rules[rs[r].selectorText] = rule;
+				}
+			}
+		}
+	}
+
+	function css($a) {
+		var cssText = '';
+		var selector;
+
+		for (var r in rules) {
+			if ( $a.is(r) ) {
+				cssText += /*rules[r].comment +* / rules[r].cssText;
+			}
+		}
+
+		if ( !cssText ) return;
+
+		if ( $a[0].id ) {
+			selector = '#' + $a[0].id;
+		} else if ( $a[0].className ) {
+			selector = $a[0].tagName + '.' + $a[0].className.split(/\s+/).join('.');
+		} else {
+			selector = $a[0].tagName;
+		}
+
+		return selector + '{' + "\n" + cssText + '}' + "\n";
+	}
+
+	this.main = function() {
+		var $els = $('body, body *');
+		var ret;
+		var res = '';
+		set_rules();
+
+		for ( var i = 0; i < $els.length; i++ ) {
+			ret = css($els.eq(i));
+			console.log( ret );
+			if ( undefined !== ret ) res += "\n" + ret;
+		}
+
+		console.log(res);
+	} */
+
+	// Short
 	var set_rules = function() {
 		var rule;
 		var sheets = document.styleSheets;
@@ -21,7 +89,7 @@ window.cwmElementStyles.load = function (window, $, undefined) {
 						//href: sheets[i].href,
 						cssText: "\t" + rs[r].style.cssText.split('; ').join(";\n\t") + "\n",
 						comment: ''
-						//comment: "\n\t" + '/* ' + sheets[i].href + ' : ' + rules[r].selectorText + ' */' + "\n"
+						//comment: "\n\t" + '/* ' + sheets[i].href + ' : ' + rules[r].selectorText + ' *\/' + "\n"
 					};
 
 					rules[rs[r].selectorText] = rule;
@@ -48,6 +116,8 @@ window.cwmElementStyles.load = function (window, $, undefined) {
 		var el = a;
 		var selector = '';
 		selector = el.tagName + '.' + el.className.split(/\s+/).join('.');
+		if ( rules_done[selector] === true ) return;
+		rules_done[selector] = true;
 
 		return selector + '{' + "\n" + cssText + '}' + "\n";
 	}
