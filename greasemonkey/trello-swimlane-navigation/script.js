fcb7260633cc3c50cf8a045732500102314b4dd4
Gerald burns <gerald@ziprecruiter.com>
2015-02-05 19:05:44 -0800

    Fixing loading of js before css is populated, reloading menu when board is switched.

--- greasemonkey/trello-swimlane-navigation/script.js
+++ greasemonkey/trello-swimlane-navigation/script.js
@@ -1,7 +1,7 @@
 ---
 ---
 
-window.cwmBaseUrl = "//{{ site.domain }}";
+window.cwmBaseUrl = "{{protocol}}//{{ site.domain }}";
 window.cwmBookmarkletUrl = window.cwmBaseUrl + "/greasemonkey/trello-swimlane-navigation";
 
 {% include js/trello-swimlane-navigation.min.js %}
--- greasemonkey/trello-swimlane-navigation/trello-swimlane-navigation.js
+++ greasemonkey/trello-swimlane-navigation/trello-swimlane-navigation.js
@@ -4,27 +4,31 @@
 	var $board_canvas;
 	var $lanes;
 	var $content;
+	var $list_cards;
+	var $trello_swimlane_navigation_css;
+	var scroll_bound = false;
 		
+	var css = '<link rel="stylesheet" id="trello-swimlane-navigation-css"/>';
+	$css = $(css)
+		.appendTo('head');
+
+	$css.attr({href: window.cwmBookmarkletUrl + '/trello-swimlane-navigation.css?1'});
+
 	var show_menu = function() {
 		if ( typeof jQuery == 'undefined' ) {
 			setTimeout(show_menu, 500);
 			return;
 		}
-		
 		(function($) { $(function() {
 			$board = $('#board');
 			$board_canvas = $('.board-canvas');
 			$content = $('#content');
 			$board_wrapper = $('.board-wrapper');
 			$lanes = $board.find('.list');
+			$list_cards = $board.find('.list-card');
+			$trello_swimlane_navigation_css = $('#trello-swimlane-navigation-css');
 
-			var css = '<link rel="stylesheet" id="trello-swimlane-navifation-css"/>';
-			$css = $(css)
-				.appendTo('head');
-
-			$css.attr({href: window.cwmBookmarkletUrl + '/trello-swimlane-navigation.css?1'});
-
-			if ( !$content.length || !$board.length || !$board_wrapper.length || !$lanes.length || !$board_canvas.length ) {
+			if ( !$trello_swimlane_navigation_css.length || $trello_swimlane_navigation_css.css('content') != 'loaded' || !$content.length || !$board_wrapper.length || !$board_canvas.length || !$board.length || !$lanes.length || !$list_cards.length ) {
 				setTimeout(show_menu, 500);
 				return;
 			}
@@ -44,7 +48,11 @@
 			//$board.prepend($lane_menu);
 			$content.prepend($lane_menu);
 			
-			$content.css('padding-left', $lane_menu.width());
+			var width = $lane_menu.width();
+
+			$content.css({
+				'padding-left': width + 10
+			});
 			
 			$lane_menu.delegate('button', 'click', function(e) {
 				e.preventDefault();
@@ -54,21 +62,32 @@
 				$board.animate({scrollLeft: scrollTo}, 'fast');
 				localStorage.setItem("scrollToLeft", scrollTo);
 			});
-			
-			var scrolling = false;
-			$board.bind('scroll', function() {
-				if ( scrolling ) return;
-				scrolling = true;
-				setTimeout(function() {
-					localStorage.setItem("scrollToLeft", $board.scrollLeft());
-					scrolling = false;
-				}, 100);
-			});
-			
-			var scrollToLeft = localStorage.getItem('scrollToLeft');
-			if ( scrollToLeft ) $board.animate({scrollLeft: scrollToLeft}, 'fast');
+
+			if ( !scroll_bound ) {
+				scroll_bound = true;
+				var scrolling = false;
+				$board.bind('scroll', function() {
+					if ( scrolling ) return;
+					scrolling = true;
+					setTimeout(function() {
+						localStorage.setItem("scrollToLeft", $board.scrollLeft());
+						scrolling = false;
+					}, 100);
+				});
+
+				var scrollToLeft = localStorage.getItem('scrollToLeft');
+				if ( scrollToLeft ) $board.animate({scrollLeft: scrollToLeft}, 'fast');
+			}
+
+			var check_me = setInterval(function() {
+				if ( !$lane_menu.parent().length ) {
+					$lane_menu.remove();
+					clearInterval(check_me);
+					show_menu();
+				}
+			}, 500);
 		})})(window.jQuery);
 	};
-	
+
 	show_menu();
 })();
