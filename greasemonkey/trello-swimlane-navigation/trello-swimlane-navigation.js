79f5a6365d956b86c34655a9aba9d746941ab0af
Gerald burns <gerald@ziprecruiter.com>
2015-02-05 19:31:04 -0800

    Adding some comments

--- greasemonkey/trello-swimlane-navigation/trello-swimlane-navigation.js
+++ greasemonkey/trello-swimlane-navigation/trello-swimlane-navigation.js
@@ -7,7 +7,8 @@
 	var $list_cards;
 	var $trello_swimlane_navigation_css;
 	var scroll_bound = false;
-		
+
+	// Add menu styling
 	var css = '<link rel="stylesheet" id="trello-swimlane-navigation-css"/>';
 	$css = $(css)
 		.appendTo('head');
@@ -15,10 +16,12 @@
 	$css.attr({href: window.cwmBookmarkletUrl + '/trello-swimlane-navigation.css?1'});
 
 	var show_menu = function() {
+		// Make sure jQuery is loaded before continuing
 		if ( typeof jQuery == 'undefined' ) {
 			setTimeout(show_menu, 500);
 			return;
 		}
+
 		(function($) { $(function() {
 			$board = $('#board');
 			$board_canvas = $('.board-canvas');
@@ -28,6 +31,7 @@
 			$list_cards = $board.find('.list-card');
 			$trello_swimlane_navigation_css = $('#trello-swimlane-navigation-css');
 
+			// Make sure board stuff exists before continuing
 			if ( !$trello_swimlane_navigation_css.length || $trello_swimlane_navigation_css.css('content') != 'loaded' || !$content.length || !$board_wrapper.length || !$board_canvas.length || !$board.length || !$lanes.length || !$list_cards.length ) {
 				setTimeout(show_menu, 500);
 				return;
@@ -35,6 +39,7 @@
 			
 			var $lane_menu = $('<menu/>').addClass('lane-menu');
 			
+			// For each lane, make a button and toss it in the menu
 			$lanes.each(function() {
 				var $this = $(this);
 				var name = $this.find('.list-header-name').text();
@@ -45,15 +50,16 @@
 				$lane_menu.append($button);
 			});
 			
-			//$board.prepend($lane_menu);
+			// Add menu to content div
 			$content.prepend($lane_menu);
-			
-			var width = $lane_menu.width();
 
+			// Move board over so menu can stay on top
+			var width = $lane_menu.width();
 			$content.css({
 				'padding-left': width + 10
 			});
-			
+
+			// Set up events on buttons to scroll to swim lanes
 			$lane_menu.delegate('button', 'click', function(e) {
 				e.preventDefault();
 				var $this = $(this);
@@ -63,6 +69,7 @@
 				localStorage.setItem("scrollToLeft", scrollTo);
 			});
 
+			// Remember last scroll position
 			if ( !scroll_bound ) {
 				scroll_bound = true;
 				var scrolling = false;
@@ -75,10 +82,12 @@
 					}, 100);
 				});
 
+				// Scroll to last position
 				var scrollToLeft = localStorage.getItem('scrollToLeft');
 				if ( scrollToLeft ) $board.animate({scrollLeft: scrollToLeft}, 'fast');
 			}
 
+			// Check to see if the board has been changed and start over if so
 			var check_me = setInterval(function() {
 				if ( !$lane_menu.parent().length ) {
 					$lane_menu.remove();
