5c992646b715323f7a9b9c2d3c1688f9815d869a
cwmonkey <wackoburns@hotmail.com>
2015-05-05 01:29:33 -0700

    Adding tooltips. Fixing show/hide menu issue.

--- greasemonkey/trello-swimlane-navigation/trello-swimlane-navigation.js
+++ greasemonkey/trello-swimlane-navigation/trello-swimlane-navigation.js
@@ -7,7 +7,7 @@
 	var $list_cards;
 	var $trello_swimlane_navigation_css;
 	var scroll_bound = false;
-	var $body = $('body');
+	var $body;
 	var hide;
 
 	// Add menu styling
@@ -20,19 +20,6 @@
 	var get_id = function() {
 		return document.location.pathname.split('/')[2];
 	};
-
-	$body
-		.delegate('.lane-menu-toggle', 'click', function(e) {
-			e.preventDefault();
-			if ( $content.is('.lane-menu-hide') ) {
-				$content.removeClass('lane-menu-hide');
-				localStorage.removeItem('lane-menu-hide');
-			} else {
-				$content.addClass('lane-menu-hide');
-				localStorage.setItem('lane-menu-hide', 1);
-			}
-		})
-		;
 
 	// Complete a promise when a selector exists
 	var exists = function(selector, wait) {
@@ -58,7 +45,172 @@
 			return;
 		}
 
-		(function($) { $(function() {
+		(function($) {$(function() {
+			////////////////////
+			// Tooltip plugin
+			///////////////////
+
+			if ( !$.fn.tooltip ) {
+/*
+
+Add accessible tooltips.
+
+Example: $('[title]').tooltip();
+
+TODO:
+  Allow for delegates other than body.
+  Add previous selectors to a single delegate bind.
+  Param class/id names.
+  Find highest zIndex?
+
+*/
+
+;(function($, undefined) {
+
+var $body = $('body');
+var $window = $(window);
+var id_name = 'tooltip';
+var class_name = 'tooltip';
+var show_class = 'tooltipShown';
+
+var $tooltip = $('<span/>')
+  .addClass(class_name)
+  .attr('id', id_name)
+  .css({
+    position: 'absolute'
+  })
+  ;
+
+var $tooltip_text = $('<span/>')
+	.addClass(class_name + '-text')
+	.appendTo($tooltip)
+	;
+
+var $tooltip_arrow = $('<span/>')
+	.addClass(class_name + '-arrow')
+	.appendTo($tooltip)
+	;
+
+var adjust_timeout;
+var show_tooltip = function() {
+  var $this = $(this);
+  var title = $this.data('title') || this.title;
+
+  if ( !title ) {
+    return;
+  }
+
+  clearTimeout(hide_timeout);
+
+  $tooltip_text.html(title);
+
+  $this
+    .data('title', title)
+    .attr({
+      'title': '',
+      'aria-describedby': id_name
+    })
+    ;
+
+  $tooltip
+  	.css({
+  		left: '',
+  		width: ''
+  	});
+
+  var offset = $this.offset();
+  var this_width = $this.outerWidth();
+  var this_height = $this.outerHeight();
+
+  $tooltip
+    .appendTo($body);
+
+  var tooltip_width = $tooltip.outerWidth();
+  $tooltip.css('width', tooltip_width);
+
+	var left = offset.left + this_width / 2 - tooltip_width / 2;
+	if ( left < 0 ) {
+		$tooltip_arrow.css('left', left);
+		left = 0;
+	} else {
+		$tooltip_arrow.css('left', 0);
+	}
+
+  $tooltip
+    .css({
+      left: left,
+      top: offset.top + this_height
+    })
+    .addClass(show_class)
+    ;
+
+
+  if ( left + $tooltip.outerWidth() > $window.width() ) {
+  	var off = (left + $tooltip.outerWidth()) - $window.width();
+  	$tooltip.css('left', left - off);
+  	$tooltip_arrow.css('left', off);
+  }
+};
+
+var hide_timeout;
+var hide_tooltip = function() {
+  var $this = $(this);
+  clearTimeout(hide_timeout);
+  hide_timeout = setTimeout(function() {
+    $this.attr('aria-describedby', '');
+    $tooltip.removeClass(show_class);
+  }, 10);
+};
+
+// Plugin
+$.fn.tooltip = function() {
+  var selector = this.selector;
+  var init = function() {
+    var ignore_next = false;
+    $body
+      /*.delegate(selector, 'mouseenter focus', show_tooltip)
+      .delegate(selector, 'mouseleave blur click mousedown mouseup', hide_tooltip)*/
+      .bind('mousemove', function(e) {
+      	if ( ignore_next ) {
+      		ignore_next = false;
+      		return;
+      	}
+
+      	var $closest = $(e.target).closest(selector);
+      	if ( !$closest.length ) {
+      		hide_tooltip();
+      	} else {
+      		show_tooltip.call($closest[0]);
+      	}
+      })
+      .bind('mousedown mouseup', function() {
+      	ignore_next = true;
+      	hide_tooltip();
+      })
+      ;
+  };
+
+  // In case this was called before the end of the document
+  if ( !$body.length ) {
+    $(function() {
+      init();
+    });
+  } else {
+    init();
+  }
+
+  return this;
+};
+
+})(window.jQuery);
+
+				$('[title]').tooltip();
+			}
+
+			////////////////////
+			// Trello Stuff
+			//////////////////
+			$body = $('body');
 			$board = $('#board');
 			$board_canvas = $('.board-canvas');
 			$content = $('#content');
@@ -79,7 +231,7 @@
 				setTimeout(show_menu, 500);
 				return;
 			}
-			
+
 			var $lane_menu = $('<div/>')
 				.addClass('lane-menu u-fancy-scrollbar')
 				;
@@ -179,8 +331,21 @@
 			var $lane_menu_wrapper = $('<div/>').addClass('lane-menu-wrapper');
 
 			$lane_menu_wrapper
-				.append('<a href="#" class="lane-menu-toggle dark-hover js-hide-sidebar" title="Show/hide swimlane navbar."><span class="icon-sm icon-forward lane-menu-open"></span><span class="icon-sm icon-forward lane-menu-closed"></span></a>')
 				.append($lane_menu)
+				;
+
+			var $toggle = $('<a href="#" class="lane-menu-toggle dark-hover js-hide-sidebar" title="Show/hide swimlane navbar."><span class="icon-sm icon-forward lane-menu-open"></span><span class="icon-sm icon-forward lane-menu-closed"></span></a>')
+				.bind('click', function(e) {
+					e.preventDefault();
+					if ( $content.is('.lane-menu-hide') ) {
+						$content.removeClass('lane-menu-hide');
+						localStorage.removeItem('lane-menu-hide');
+					} else {
+						$content.addClass('lane-menu-hide');
+						localStorage.setItem('lane-menu-hide', 1);
+					}
+				})
+				.prependTo($lane_menu_wrapper)
 				;
 
 			$content.prepend($lane_menu_wrapper);
