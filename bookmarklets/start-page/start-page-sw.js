6bef6b89ecb04bfe056fb57cb96266310c99fb97
cwmonkey <wackoburns@hotmail.com>
2016-05-26 19:31:22 -0700

    Caching edit page

--- bookmarklets/start-page/start-page-sw.js
+++ bookmarklets/start-page/start-page-sw.js
@@ -1,8 +1,11 @@
 this.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open('v1').then(function(cache) {
+    caches.open('v2').then(function(cache) {
       return cache.addAll([
-        '/bookmarklets/start-page/start-page.html'
+        '/bookmarklets/start-page/start-page.html',
+        '/bookmarklets/start-page/start-page-edit.html',
+        '/bookmarklets/start-page/start-page-edit.js',
+        '/bookmarklets/start-page/start-page-edit.css'
       ]);
     })
   );
@@ -12,7 +15,7 @@ this.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request).catch(function() {
       return fetch(event.request).then(function(response) {
-        return caches.open('v1').then(function(cache) {
+        return caches.open('v2').then(function(cache) {
           cache.put(event.request, response.clone());
           return response;
         });
