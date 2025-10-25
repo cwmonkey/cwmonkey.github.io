3a14767d8064f538b38f05c9525c703ffc462550
cwmonkey <wackoburns@hotmail.com>
2025-10-25 15:59:00 -0700

    Only target relevant url's

--- _includes/greasemonkey/sa-remove-subforum-stylesheets/meta.js
+++ _includes/greasemonkey/sa-remove-subforum-stylesheets/meta.js
@@ -4,7 +4,10 @@
 // @version      {% include greasemonkey/sa-remove-subforum-stylesheets/version %}
 // @description  Remove subforum stylesheet overrides
 // @author       WrasslorMonkey
-// @match        https://forums.somethingawful.com/*
+// @match        https://forums.somethingawful.com/showthread.php*
+// @match        https://forums.somethingawful.com/editpost.php*
+// @match        https://forums.somethingawful.com/newreply.php*
+// @match        https://forums.somethingawful.com/forumdisplay.php*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=somethingawful.com
 // @grant        none
 // @run-at       document-start
