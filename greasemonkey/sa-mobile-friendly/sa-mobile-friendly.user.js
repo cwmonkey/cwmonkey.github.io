69a07c113eb79d495211884774b7e08d6069a851
cwmonkey <wackoburns@hotmail.com>
2025-10-27 22:36:17 -0700

    Add show/hide nav button and other styling tweaks

--- greasemonkey/sa-mobile-friendly/sa-mobile-friendly.user.js
+++ greasemonkey/sa-mobile-friendly/sa-mobile-friendly.user.js
@@ -5,11 +5,13 @@
 (function() {
   'use strict';
 
+  // Add mobile stylesheet
   const style = document.createElement("STYLE");
   style.textContent = `{% include greasemonkey/sa-mobile-friendly/sa-mobile-friendly.css %}`;
 
   document.documentElement.append(style);
 
+  // Add mobile meta tag
   function addMeta() {
     const head = document.querySelector('head');
     const meta = document.createElement('meta');
@@ -29,4 +31,36 @@
   }
 
   tryAddMeta();
+
+  // Add mobile menu button
+  let $container = null;
+  const bodyOpenClass = 'sa-mobile-friendly-menu-button-clicked';
+
+  function addMobileButton() {
+    const $button = document.createElement('BUTTON');
+    $button.className = 'sa-mobile-friendly-menu-button';
+    $button.textContent = 'Show Navigation';
+
+    $button.addEventListener('click', () => {
+      if (document.body.classList.contains(bodyOpenClass)) {
+        document.body.classList.remove(bodyOpenClass);
+      } else {
+        document.body.classList.add(bodyOpenClass);
+      }
+    });
+
+    $container.prepend($button);
+  }
+
+  function tryAddMobileButton() {
+    $container = document.querySelector('#container');
+
+    if ($container) {
+      addMobileButton();
+    } else {
+      setTimeout(tryAddMeta, 100);
+    }
+  }
+
+  tryAddMobileButton();
 })();
