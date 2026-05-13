39e4520fcffc1b107ade70e1786d8d8132e1c8d6
cwmonkey <wackoburns@hotmail.com>
2026-05-12 20:57:14 -0700

    Remove js required scripts for now.

--- bookmarklets/forum-update/bookmarklet.js
+++ bookmarklets/forum-update/bookmarklet.js
@@ -11,49 +11,6 @@ window.cwmBookmarkletUrl = window.cwmBaseUrl + "/bookmarklets/forum-update";
 
 // Load scripts
 cwmJsload.load(
-	[{
-		script: '//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js',
-		static: true,
-		test: function() {
-			var v1 = 1;
-			var v2 = 8;
-			var v3 = 1;
-			var hasJquery = false;
-
-			if ( typeof window.jQueryCwm !== 'undefined' ) return true;
-
-			if ( typeof window.jQuery !== 'undefined' ) {
-				var vs = window.jQuery.fn.jquery.split('.');
-				if ( vs[0] > v1 ) return true;
-				if ( vs[0] < v1 ) return false;
-
-				if ( vs[1] > v2 ) return true;
-				if ( vs[1] < v2 ) return false;
-
-				if ( (vs[2] || 0) >= v3 ) return true;
-
-				return false;
-			}
-
-			return false;
-		},
-		callback: function() {
-			window.jQueryCwm = jQuery.noConflict(true);
-			var jQueryOriginal = jQuery || window.jQueryCwm;
-		}
-	},
-	{
-		script: window.cwmBaseUrl + '/js/vendor/jQuery.scrollTo/2.1.2/jquery.scrollTo.js',
-		static: true,
-		test: function() {
-			return !!jQuery.fn.scrollTo;
-		},
-		callback: function() {
-			if ( window.jQueryCwm && !window.jQueryCwm.fn.scrollTo ) {
-				window.jQueryCwm.fn.scrollTo = window.jQuery.fn.scrollTo;
-			}
-		}
-	}],
 	{
 		script: window.cwmBaseUrl + '/bookmarklets/forum-update/forum-update.js',
 		static: true,
